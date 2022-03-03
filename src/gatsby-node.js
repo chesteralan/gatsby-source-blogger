e1fb52fbc7f753cebe1b7f8045630bd093638166
Chester Alan <chesteralantagudin@gmail.com>
2022-03-03 18:37:51 +0800

    updates

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -4,86 +4,91 @@ const parse = require("rehype-parse");
 const rehype2remark = require("rehype-remark");
 const stringify = require("remark-stringify");
 const crypto = require("crypto");
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
 const typePrefix = "blogger__";
 
 const refactoredEntityTypes = {
   post: `${typePrefix}POST`,
-  page: `${typePrefix}PAGE`
+  page: `${typePrefix}PAGE`,
 };
 
-exports.sourceNodes = async ({ cache, store, actions, createNodeId }, { apiKey, blogId }) => {
+exports.sourceNodes = async (
+  { cache, store, actions, createNodeId },
+  { apiKey, blogId, fetch_posts, fetch_pages, limit }
+) => {
   const { createNode, setPluginStatus } = actions;
 
   const blogger = google.blogger({
     version: "v3",
-    auth: apiKey
+    auth: apiKey,
   });
 
-  let postResult;
-  let posts = [];
+  const _fetch_posts = fetch_posts || true;
 
-  try {
-    let params = {
-      blogId: blogId,
-      maxResults: 500,
-      fetchImages: true
-    };
+  if (_fetch_posts) {
+    let postResult;
+    let posts = [];
 
-    do {
-      postResult = await blogger.posts.list(params);
-      if (postResult.data.nextPageToken) {
-        params = { ...params, pageToken: postResult.data.nextPageToken };
-      }
-      if (postResult.data.items) {
-        posts.push(...postResult.data.items);
-      }
-    } while (postResult.data.nextPageToken);
-  } catch (err) {
-    console.log("Error fetching posts", err);
-  }
+    try {
+      let params = {
+        blogId: blogId,
+        maxResults: limit || 500,
+        fetchImages: true,
+      };
 
-  const rePost = /^https?:\/\/(?:[^/]+)\/\d{4}\/\d{2}\/([^/][^.]+)\.html$/;
-
-  if (posts) {
-    posts.forEach(async post => {
-      let featuredImageNode = null
-
-      if (post.images)
-      {
-        try {
-          featuredImageNode = await createRemoteFileNode({
-            url: post.images[0].url,
-            store,
-            cache,
-            createNode,
-            createNodeId,
-          })        
-        } catch (e) {
-          throw Error(e);
+      do {
+        postResult = await blogger.posts.list(params);
+        if (postResult.data.nextPageToken) {
+          params = { ...params, pageToken: postResult.data.nextPageToken };
         }
-      }      
+        if (postResult.data.items) {
+          posts.push(...postResult.data.items);
+        }
+      } while (postResult.data.nextPageToken);
+    } catch (err) {
+      console.log("Error fetching posts", err);
+    }
 
-      if (featuredImageNode && featuredImageNode.ext !== ".gif") {
-        post.featuredImage___NODE = featuredImageNode.id
-      }
+    const rePost = /^https?:\/\/(?:[^/]+)\/\d{4}\/\d{2}\/([^/][^.]+)\.html$/;
 
-      return unified()
-        .use(parse)
-        .use(rehype2remark)
-        .use(stringify)
-        .process(post.content, function(err, md) {
-          if (err) console.log(err);
-          const segments = rePost.exec(post.url);
-          const gatsbyPost = Object.assign({ slug: segments[1] }, post, {
-            id: createNodeId(post.id),
-            parent: null,
-            children: [],
-            internal: {
-              type: refactoredEntityTypes.post,
-              mediaType: `text/markdown`,
-              content: `---
+    if (posts) {
+      posts.forEach(async (post) => {
+        let featuredImageNode = null;
+
+        if (post.images) {
+          try {
+            featuredImageNode = await createRemoteFileNode({
+              url: post.images[0].url,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+            });
+          } catch (e) {
+            throw Error(e);
+          }
+        }
+
+        if (featuredImageNode && featuredImageNode.ext !== ".gif") {
+          post.featuredImage___NODE = featuredImageNode.id;
+        }
+
+        return unified()
+          .use(parse)
+          .use(rehype2remark)
+          .use(stringify)
+          .process(post.content, function (err, md) {
+            if (err) console.log(err);
+            const segments = rePost.exec(post.url);
+            const gatsbyPost = Object.assign({ slug: segments[1] }, post, {
+              id: createNodeId(post.id),
+              parent: null,
+              children: [],
+              internal: {
+                type: refactoredEntityTypes.post,
+                mediaType: `text/markdown`,
+                content: `---
 title: >-
   ${post.title}
 date: ${post.published}
@@ -92,59 +97,63 @@ featuredImageUrl: ${post.images ? post.images[0].url : null}
 ---
 
 ${md}`,
-              contentDigest: crypto
-                .createHash(`md5`)
-                .update(JSON.stringify(post))
-                .digest(`hex`)
-            }
+                contentDigest: crypto
+                  .createHash(`md5`)
+                  .update(JSON.stringify(post))
+                  .digest(`hex`),
+              },
+            });
+
+            createNode(gatsbyPost);
           });
-
-          createNode(gatsbyPost);
-        });
-    });
+      });
+    }
   }
 
-  let pageResult;
-  let pages = [];
+  const _fetch_pages = fetch_pages || false;
 
-  try {
-    let params = {
-      blogId: blogId,
-      maxResults: 500
-    };
+  if (_fetch_pages) {
+    let pageResult;
+    let pages = [];
 
-    do {
-      pageResult = await blogger.pages.list(params);
-      if (pageResult.data.nextPageToken) {
-        params = { ...params, pageToken: pageResult.data.nextPageToken };
-      }
-      if (pageResult.data.items) {
-        pages.push(...pageResult.data.items);
-      }
-    } while (pageResult.data.nextPageToken);
-  } catch (err) {
-    console.log("Error fetching pages", err);
-  }
+    try {
+      let params = {
+        blogId: blogId,
+        maxResults: limit || 500,
+      };
 
-  const rePage = /^https?:\/\/(?:[^/]+)\/p\/([^/][^.]+)\.html$/;
+      do {
+        pageResult = await blogger.pages.list(params);
+        if (pageResult.data.nextPageToken) {
+          params = { ...params, pageToken: pageResult.data.nextPageToken };
+        }
+        if (pageResult.data.items) {
+          pages.push(...pageResult.data.items);
+        }
+      } while (pageResult.data.nextPageToken);
+    } catch (err) {
+      console.log("Error fetching pages", err);
+    }
 
-  if (pages) {
-    pages.forEach(page => {
-      unified()
-        .use(parse)
-        .use(rehype2remark)
-        .use(stringify)
-        .process(page.content, function(err, md) {
-          if (err) console.log(err);
-          const segments = rePage.exec(page.url);
-          const gatsbyPage = Object.assign({ slug: segments[1] }, page, {
-            id: createNodeId(page.id),
-            parent: null,
-            children: [],
-            internal: {
-              type: refactoredEntityTypes.page,
-              mediaType: `text/markdown`,
-              content: `---
+    const rePage = /^https?:\/\/(?:[^/]+)\/p\/([^/][^.]+)\.html$/;
+
+    if (pages) {
+      pages.forEach((page) => {
+        unified()
+          .use(parse)
+          .use(rehype2remark)
+          .use(stringify)
+          .process(page.content, function (err, md) {
+            if (err) console.log(err);
+            const segments = rePage.exec(page.url);
+            const gatsbyPage = Object.assign({ slug: segments[1] }, page, {
+              id: createNodeId(page.id),
+              parent: null,
+              children: [],
+              internal: {
+                type: refactoredEntityTypes.page,
+                mediaType: `text/markdown`,
+                content: `---
   title: >-
     ${page.title}
   date: ${page.published}
@@ -152,21 +161,22 @@ ${md}`,
   ---
 
   ${md}`,
-              contentDigest: crypto
-                .createHash(`md5`)
-                .update(JSON.stringify(page))
-                .digest(`hex`)
-            }
+                contentDigest: crypto
+                  .createHash(`md5`)
+                  .update(JSON.stringify(page))
+                  .digest(`hex`),
+              },
+            });
+
+            createNode(gatsbyPage);
           });
 
-          createNode(gatsbyPage);
+        setPluginStatus({
+          status: {
+            lastFetched: Date.now(),
+          },
         });
-
-      setPluginStatus({
-        status: {
-          lastFetched: Date.now()
-        }
       });
-    });
+    }
   }
 };
